8d9377ab1276b971dfcc870b6056531b883d7f92
Percy-Potter2008 <75735895+Percy-Potter2008@users.noreply.github.com>
2021-10-11 19:36:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,19 +16,8 @@ function setup() {
 function modelLoaded() {
   console.log('PoseNet is intialized!');
 }
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-    console.log('results');
-    
-     leftWristX = results[0].pose.leftWrist.x;
-    rightWristX = results[0].pose.rightWrist.x;
-    difference = floor(leftWristX - rightWristX);
-    
-    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
-  }
-}
+function gotPoses(results) { if(results.length > 0) { console.log(results); noseX = results[0].pose.nose.x; noseY = results[0].pose.nose.y; console.log("noseX = " + noseX +" noseY = " + noseY); leftWristX = results[0].pose.leftWrist.x; rightWristX = results[0].pose.rightWrist.x; difference = floor(leftWristX - rightWristX); console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference); } }results');
+
 function draw() {
   background('#969A97');
   
